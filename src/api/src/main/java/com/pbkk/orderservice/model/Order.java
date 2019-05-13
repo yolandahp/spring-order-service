9df23fec7e281dff9f185be879cc0ca945cfdd43
dandynaufaldi <dandy.naufaldi@gmail.com>
2019-05-13 15:05:08 +0000

    fix bug deals

--- src/api/src/main/java/com/pbkk/orderservice/model/Order.java
+++ src/api/src/main/java/com/pbkk/orderservice/model/Order.java
@@ -61,9 +61,6 @@ public class Order {
 	@Column(name = "deals")
 	private Double deals;
 	
-	@Column(name = "deals")
-	private Double deals;
-	
 	@Column(name = "total_price")
 	private Double totalPrice;
 	
